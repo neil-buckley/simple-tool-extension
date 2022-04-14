e74e4ee81c829acabeb37e6b97a1f8decd981797
Neil Buckley <neil@lphmedia.com>
2022-04-14 11:30:40 +0100

    fix: updated default port

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
-const PORT = 3000
+const PORT = 1111
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
