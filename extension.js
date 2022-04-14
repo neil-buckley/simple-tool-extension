9f058bbd17b8872fc2b21b714097df17fa3936a9
Neil Buckley <neil@lphmedia.com>
2022-04-14 11:03:25 +0100

    chore: tidy up unused stuff

--- extension.js
+++ extension.js
@@ -18,53 +18,28 @@ function activate(context) {
       provider
     )
   )
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  // let disposable = vscode.commands.registerCommand(
-  //   'simple-tool.simpleTool',
-  //   function () {
-  //     // The code you place here will be executed every time your command is executed
-
-  //     // Display a message box to the user
-  //     vscode.window.showInformationMessage('Hello World VS code!')
-  //   }
-  // )
-
-  // context.subscriptions.push(disposable)
 }
 
 // taken from: https://github.com/microsoft/vscode-extension-samples/blob/main/webview-view-sample/ and converted from TS
-
 var SimpleTooleViewProvider = /** @class */ (function () {
   function SimpleTooleViewProvider(_extensionUri) {
     this._extensionUri = _extensionUri
   }
   SimpleTooleViewProvider.prototype.resolveWebviewView = function (
-    webviewView,
-    context,
-    _token
+    webviewView
   ) {
     this._view = webviewView
     webviewView.webview.options = {
       // Allow scripts in the webview
       enableScripts: true,
       localResourceRoots: [this._extensionUri],
-      // This maps localhost:3000 in the webview to the express server port on the remote host.
+      // This maps localhost:3000 in the webview to a server port on the remote/external host.
       portMapping: [{ webviewPort: PORT, extensionHostPort: PORT }],
     }
-    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview)
+    webviewView.webview.html = this._getHtmlForWebview()
   }
 
-  SimpleTooleViewProvider.prototype._getHtmlForWebview = function (webview) {
-    // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-    // var scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
-    // // Do the same for the stylesheet.
-    // var styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
-    // var styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
-    // var styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
-
+  SimpleTooleViewProvider.prototype._getHtmlForWebview = function () {
     return `<!DOCTYPE html>
             <html lang="en"">
             <head>
