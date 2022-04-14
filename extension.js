fb9556bfdee4fd968894162188d0a781a1ad3e01
Neil Buckley <neil@lphmedia.com>
2022-04-14 17:46:02 +0100

    fix: rendering problem

--- extension.js
+++ extension.js
@@ -4,61 +4,55 @@ const vscode = require('vscode')
 const PORT = vscode.workspace.getConfiguration('simple').get('port', 11011)
 
 //TODO: run the viewer here directly from published package
-
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  const provider = new SimpleTooleViewProvider(context.extensionUri)
+  const provider = new SimpleToolViewProvider(context.extensionUri)
   context.subscriptions.push(
     vscode.window.registerWebviewViewProvider(
-      SimpleTooleViewProvider.viewType,
+      SimpleToolViewProvider.viewType,
       provider,
-      { webviewOptions: { retainContextWhenHidden: true, enableScripts: true } }
+      { webviewOptions: { retainContextWhenHidden: true } }
     )
   )
-
-  console.log(getProjectRootPath())
 }
 
-function getProjectRootPath() {
-  let workspace = vscode.workspace
+// console.log(getProjectRootPath())
+// function getProjectRootPath() {
+//   let workspace = vscode.workspace
 
-  //Note: Doesn't cater for multiple open workspaces, default to the first
-  return workspace.workspaceFolders
-    ? workspace.workspaceFolders[0].uri.fsPath
-    : workspace.rootPath
-}
+//   //Note: Doesn't cater for multiple open workspaces, default to the first
+//   return workspace.workspaceFolders
+//     ? workspace.workspaceFolders[0].uri.fsPath
+//     : workspace.rootPath
+// }
+
+// TODO: open a file a file etc. we may need to relay from the iframe to the holding page and up
+// e.g. vscode.workspace.openTextDocument(Uri.file(path))
+// webviewView.webview.onDidReceiveMessage(data => {
+// 	console.log({data})
+// });
 
 // taken from: https://github.com/microsoft/vscode-extension-samples/blob/main/webview-view-sample/
-// and converted from TS -  defo could be simplified
-var SimpleTooleViewProvider = /** @class */ (function () {
-  function SimpleTooleViewProvider(_extensionUri) {
+var SimpleToolViewProvider = /** @class */ (function () {
+  function SimpleToolViewProvider(_extensionUri) {
     this._extensionUri = _extensionUri
   }
-  SimpleTooleViewProvider.prototype.resolveWebviewView = function (
-    webviewView
-  ) {
-    this._view = webviewView
+  SimpleToolViewProvider.prototype.resolveWebviewView = function (webviewView) {
+    this._webview = webviewView
     webviewView.webview.options = {
       enableScripts: true,
-      retainContextWhenHidden: true,
       localResourceRoots: [this._extensionUri],
       portMapping: [{ webviewPort: PORT, extensionHostPort: PORT }],
     }
-    webviewView.webview.html = this._getHtmlForWebview()
 
-    // TODO: open a file a file etc. we may need to relay from the iframe to the holding page and up
-    // e.g. vscode.workspace.openTextDocument(Uri.file(path))
-    // webviewView.webview.onDidReceiveMessage(data => {
-    // 	console.log({data})
-    // });
+    webviewView.webview.html = this._getHtmlForWebview()
   }
 
-  SimpleTooleViewProvider.prototype._getHtmlForWebview = function () {
+  SimpleToolViewProvider.prototype._getHtmlForWebview = function () {
     return `<!DOCTYPE html>
-            <html lang="en"">
+            <html lang="en">
             <head>
                 <meta charset="UTF-8">
-                <title>Simple/title>
+                <title>Preview</title>
                 <style>
                     html { width: 100%; height: 100%; min-height: 100%; display: flex; }
                     body { flex: 1; display: flex; }
@@ -70,8 +64,9 @@ var SimpleTooleViewProvider = /** @class */ (function () {
             </body>
             </html>`
   }
-  SimpleTooleViewProvider.viewType = 'simple-tool.simpleToolView'
-  return SimpleTooleViewProvider
+
+  SimpleToolViewProvider.viewType = 'simple-tool.simpleToolView'
+  return SimpleToolViewProvider
 })()
 
 // this method is called when your extension is deactivated
